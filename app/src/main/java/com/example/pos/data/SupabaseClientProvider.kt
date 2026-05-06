9d0a71323a6a4592d03c7ad19e4af82e997066cd
Ariel2711 <arielnaviandanaputra@gmail.com>
2026-05-06 19:48:16 +0700

    chore: set Supabase URL and API key

--- app/src/main/java/com/example/pos/data/SupabaseClientProvider.kt
+++ app/src/main/java/com/example/pos/data/SupabaseClientProvider.kt
@@ -10,8 +10,8 @@ object SupabaseClientProvider {
      * Ini mirip singleton sederhana di Kotlin.
      */
     val client = createSupabaseClient(
-        supabaseUrl = "https://jsigjqypwkjtkckjqarw.supabase.co",
-        supabaseKey = "sb_publishable_SmbbMw_Lm8fd1rWxYKOrXw_07G4bTqr"
+        supabaseUrl = "https://zpznnyalnyycyazuozqx.supabase.co",
+        supabaseKey = "sb_publishable_Eg3ke7eWZpDbOz-DHEq9IA_AUmQBmyq"
     ) {
         /*
          * install(Auth) digunakan agar aplikasi bisa memakai fitur autentikasi,
