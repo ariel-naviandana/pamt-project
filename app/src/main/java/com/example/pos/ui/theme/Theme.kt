329fe273b920c4fe79cfa216223deb5d443f61d7
mivanf <michaelivanf@gmail.com>
2026-05-12 22:46:49 +0700

    Fix Theme.kt

--- app/src/main/java/com/example/pos/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pos/ui/theme/Theme.kt
@@ -1,47 +1,97 @@
 package com.example.pos.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
-    primary = GreenPrimary,          // Warna utama (untuk Button, dll)
-    onPrimary = White,               // Warna teks di atas warna primary
-    background = GreenPrimary,       // Kita buat background utamanya hijau
-    onBackground = White,            // Warna teks di atas background
-    surface = GreenPrimary,
+
+    // Primary
+    primary = GreenPrimary,
+    onPrimary = White,
+
+    // Primary Container
+    primaryContainer = GreenContainer,
+    onPrimaryContainer = GreenDark,
+
+    // Secondary
+    secondary = GreenPrimary,
+    onSecondary = White,
+
+    secondaryContainer = GreenContainer,
+    onSecondaryContainer = GreenDark,
+
+    // Tertiary
+    tertiary = GreenPrimary,
+    onTertiary = White,
+
+    tertiaryContainer = GreenContainer,
+    onTertiaryContainer = GreenDark,
+
+    // Background
+    background = BackgroundLight,
+    onBackground = Color.Black,
+
+    // Surface
+    surface = White,
+    onSurface = Color.Black,
+
+    surfaceVariant = Color(0xFFE8F5E9),
+    onSurfaceVariant = GreenDark,
+
+    // Error
+    error = Color(0xFFB3261E),
+    onError = White,
+
+    // Outline
+    outline = Color(0xFFB7CBB2),
+
+    // NAVBAR/FAB
+    inversePrimary = GreenPrimary
+)
+
+private val DarkColorScheme = darkColorScheme(
+
+    primary = GreenPrimary,
+    onPrimary = White,
+
+    primaryContainer = GreenDark,
+    onPrimaryContainer = White,
+
+    secondary = GreenPrimary,
+    onSecondary = White,
+
+    tertiary = GreenPrimary,
+    onTertiary = White,
+
+    background = Color(0xFF121212),
+    onBackground = White,
+
+    surface = Color(0xFF131313),
     onSurface = White,
-    primaryContainer = White,        // Untuk kontras Button jika ingin warna putih
-    onPrimaryContainer = GreenPrimary
+
+    surfaceVariant = Color(0xFF2C2C2C),
+    onSurfaceVariant = White,
+
+    inversePrimary = GreenPrimary,
 )
 
 @Composable
 fun SupabaseAuthComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Fitur Dynamic Color (Android 12+)
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+
+    val colorScheme =
+        if (darkTheme) {
+            DarkColorScheme
+        } else {
+            LightColorScheme
         }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
